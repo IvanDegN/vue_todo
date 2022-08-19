1c266bbf68705df3afdfdaaddacefff9464d7227
IvanDegN <mauzer.bruno@yandex.ru>
2022-08-19 08:25:43 +0300

    Изменены файлы router.js, vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
 
-module.exports={
-  publicPath: '/vue_todo/'
+
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue_todo/'
+        : '/',
 }
